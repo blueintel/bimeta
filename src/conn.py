460659d043f6ed5dc1a38662c40bd2b2a4847540
BlueIntel <blueintel@localhost>
2021-05-18 10:24:26 -0400

    verify required config entries in bimeta_config.toml

--- src/conn.py
+++ src/conn.py
@@ -5,10 +5,26 @@ import ssl
 
 def get_connection(configFile):
     config = toml.load(configFile)
+
+    # do we have keys we need
+    main = config.get("bimeta_client")
+    if main is None:
+        print("Unable to find bimeta_client key in config file. Please see bimeta_config.toml.example")
+        exit(1)
+    if not "port" in main:
+        print("Unable to find port entry in config file under bimeta_client. Please see bimeta_config.toml.example")
+        exit(1)
+    if not "host" in main:
+        print("Unable to find host entry in config file under bimeta_client. Please see bimeta_config.toml.example")
+        exit(1)
+    if not "apikey" in main:
+        print("Unable to find apikey entry in config file under bimeta_client. Please see bimeta_config.toml.example")
+        exit(1)
+
     # set up connection parms
-    port = config["bimeta_client"]["port"]
-    host = config["bimeta_client"]["host"]
-    key = config["bimeta_client"]["apikey"]
+    port = main["port"]
+    host = main["host"]
+    key = main["apikey"]
     # get ssl cert for channel...
     cert = ssl.get_server_certificate((host, port))
     cert_bytes = bytes(cert, 'utf-8')
